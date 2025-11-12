bcc4a728c305d9e38ad6cf6b80bdb958b3bf790a
T1AG01 <tiagotlucena@gmail.com>
2025-11-12 11:39:28 -0300

    a

--- app.test.js
+++ app.test.js
@@ -1,2 +1,14 @@
 let calcular = require('./app');
 
+describe('Funções de calculo', function (){
+    test('Calcular a média entre 2 números', function(){
+        expect(calcular.somar(5,15).toBe(20));
+
+
+    });
+    test('Calcular a soma entre 2 números', function(){
+        expect(calcular.media(20,10).toBe(15));
+
+    })
+
+})
